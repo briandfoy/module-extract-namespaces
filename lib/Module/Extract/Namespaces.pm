e9723284d90be2e319e69f3d831489f2f1523891
brian d foy <brian.d.foy@gmail.com>
2015-01-03 17:05:18 -0500

    Update copyright statement

--- lib/Module/Extract/Namespaces.pm
+++ lib/Module/Extract/Namespaces.pm
@@ -651,7 +651,7 @@ created this module.
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (c) 2008-2014, brian d foy, All Rights Reserved.
+Copyright © 2008-2015, brian d foy <bdfoy@cpan.org>. All rights reserved.
 
 You may redistribute this under the same terms as Perl itself.
 
