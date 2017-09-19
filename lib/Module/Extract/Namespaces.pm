4a74aa4e6ac929077f2a0b1ae88ace77f0c916d6
brian d foy <brian.d.foy@gmail.com>
2017-09-19 15:41:25 -0500

    Clarify that I use Artistic 2.0

--- lib/Module/Extract/Namespaces.pm
+++ lib/Module/Extract/Namespaces.pm
@@ -351,7 +351,7 @@ created this module.
 
 Copyright © 2008-2015, brian d foy <bdfoy@cpan.org>. All rights reserved.
 
-You may redistribute this under the same terms as Perl itself.
+You may redistribute this under the Artistic License 2.0.
 
 =cut
 
